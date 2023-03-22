cc64e46d80609293bb1a2d800462e36f87c0472f
Pierre Noël <petersg83@gmail.com>
2023-03-22 12:16:06 +0100

    add authentication check on api/generate-basecamp-members route

--- routes/api/generate-basecamp-members.js
+++ routes/api/generate-basecamp-members.js
@@ -1,6 +1,11 @@
+import isAuthenticated from "../../methods/auth/isAuthenticated.js";
 import getBasecampMembersAndMetadata from '../../methods/members/getBasecampMembersAndMetadata.js';
 
-export const handler = async (_req, _ctx) => {
+export const handler = async (req) => {
+  if (!isAuthenticated(req)) {
+    return new Response(null, { status: 401 });
+  }
+
   await getBasecampMembersAndMetadata({ method: 'regenerate' });
 
   return new Response();
